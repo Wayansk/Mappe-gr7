ad51e1aca2f492f6660d1c5eb59bfe28a8e9c844
Wayan <wayan@krabseth.no>
2025-01-29 14:15:05 +0100

    created dice concept and test file

--- src/main/java/gameplay/Dice.java
+++ src/main/java/gameplay/Dice.java
@@ -1,8 +1,19 @@
 package gameplay;
 
+import java.util.Random;
+
 public class Dice {
+  private Random random;
 
+  public Dice() {
+    this.random = new Random();
+  }
 
-
-
+  /**
+   * Rolls the dice and returns a value between 1 and 6.
+   * @return A random integer between 1 and 6.
+   */
+  public int roll() {
+    return random.nextInt(6) + 1;
+  }
 }
---
+++ src/test/java/gameplayTest/DiceRollTest.java
@@ -0,0 +1,17 @@
+package gameplayTest;
+
+import gameplay.Dice;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DiceRollTest {
+  @Test
+  public void testDiceRollWithinRange() {
+    Dice dice = new Dice();
+    for (int i = 0; i < 50; i++) { // Roll multiple times
+      int roll = dice.roll();
+      System.out.println("Roll #" + (i + 1) + ": " + roll);
+      assertTrue(roll >= 1 && roll <= 6, "Dice roll out of range: " + roll);
+    }
+  }
+}
