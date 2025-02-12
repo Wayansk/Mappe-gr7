ae8404bdaac4f2e5f3624abd29be0fae24954c9e
rikarsv <rikardsv@stud.ntnu.no>
2025-02-12 14:08:04 +0100

    Created the first methods in Pieces class
    Only created the first methods, including constructor, commited other files as well.

--- src/main/java/gameplay/Pieces.java
+++ src/main/java/gameplay/Pieces.java
@@ -1,4 +1,26 @@
 package gameplay;
 
 public class Pieces {
+
+    private int gamePieces;
+
+    public Pieces(int gamePieces){
+        setNumberOfPieces(gamePieces);
+    }
+
+
+
+
+
+
+
+
+    public void setNumberOfPieces(int gamePieces){
+        this.gamePieces = gamePieces;
+    }
+
+    public int getPieces(){
+        return gamePieces;
+    }
+
 }
