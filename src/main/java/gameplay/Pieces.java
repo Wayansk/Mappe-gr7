4fd2753537de1cbc85b7a09d019c638ee8cca2a2
rikarsv <rikardsv@stud.ntnu.no>
2025-02-13 21:45:15 +0100

    Added new features in Pieces
    Created setters for both name, and color. Added also 10 color, for up to 10 players. Wrote enums for the different colors, that can be used for the GUI in JavaFX. I also added some Javadoc.

--- src/main/java/gameplay/Pieces.java
+++ src/main/java/gameplay/Pieces.java
@@ -1,26 +1,101 @@
 package gameplay;
 
+import javafx.scene.paint.Color;
+
+/**
+ * Represents a game piece with a specific name, color, and quantity.
+ * <p>
+ * This class is used to manage game pieces in a gameplay scenario.
+ * </p>
+ */
 public class Pieces {
 
-    private int gamePieces;
+    /**
+     * Enumeration of possible colors for game pieces.
+     * <p>
+     * Each enum constant is associated with a JavaFX {@link Color} instance.
+     * </p>
+     */
+    public enum PieceColor {
+        RED(Color.RED),
+        BLUE(Color.BLUE),
+        GREEN(Color.GREEN),
+        YELLOW(Color.YELLOW),
+        VIOLET(Color.VIOLET),
+        PINK(Color.PINK),
+        WHITE(Color.WHITE),
+        BLACK(Color.BLACK),
+        ORANGE(Color.ORANGE),
+        PURPLE(Color.PURPLE);
 
-    public Pieces(int gamePieces){
-        setNumberOfPieces(gamePieces);
+        private final Color fxColor;
+
+        /**
+         * Constructs a {@code PieceColor} with the specified JavaFX color.
+         *
+         * @param fxColor the JavaFX {@link Color} associated with this piece color.
+         */
+        PieceColor(Color fxColor) {
+            this.fxColor = fxColor;
+        }
+
+        /**
+         * Returns the JavaFX {@link Color} associated with this piece color.
+         *
+         * @return the JavaFX {@link Color} of this piece color.
+         */
+        public Color getFxColor() {
+            return fxColor;
+        }
     }
 
+    private String nameOfPiece;
+    private PieceColor color;
 
-
-
-
-
-
-
-    public void setNumberOfPieces(int gamePieces){
-        this.gamePieces = gamePieces;
+    /**
+     * Constructs a {@code Pieces} instance with the specified number of pieces, name, and color.
+     *
+     * @param nameOfPiece the name of the piece.
+     * @param color       the {@link PieceColor} of the piece.
+     */
+    public Pieces(String nameOfPiece, PieceColor color) {
+        setNameOfPiece(nameOfPiece);
+        setColorOfPiece(color);
     }
 
-    public int getPieces(){
-        return gamePieces;
+    /**
+     * Sets the name of the piece.
+     *
+     * @param nameOfPiece the name to assign to the piece.
+     */
+    public void setNameOfPiece(String nameOfPiece) {
+        this.nameOfPiece = nameOfPiece;
     }
 
+    /**
+     * Sets the color of the piece.
+     *
+     * @param color the {@link PieceColor} to assign to the piece.
+     */
+    public void setColorOfPiece(PieceColor color) {
+        this.color = color;
+    }
+
+    /**
+     * Returns the name of the piece.
+     *
+     * @return the name of the piece.
+     */
+    public String getNameOfPiece() {
+        return nameOfPiece;
+    }
+
+    /**
+     * Returns the color of the piece.
+     *
+     * @return the {@link PieceColor} of the piece.
+     */
+    public PieceColor getColorOfPiece() {
+        return color;
+    }
 }
