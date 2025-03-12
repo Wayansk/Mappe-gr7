47bfd82f18a4dd7c0495b0c1222f5984a24c1070
rikardsv <rikardsv@stud.ntnu.no>
2025-03-12 11:37:12 +0100

    New class BoardGame with the new changes to Player-class
    Merged Player-branch with Board-Game branch

---
+++ src/main/java/engine/BoardGame.java
@@ -0,0 +1,16 @@
+package engine;
+
+import board.Board;
+import gameplay.Dice;
+import gameplay.Player;
+
+import java.util.List;
+
+public class BoardGame {
+
+    private Board board;
+    private Player currentplayer;
+    private List<Player> players;
+    private Dice dice;
+
+}
