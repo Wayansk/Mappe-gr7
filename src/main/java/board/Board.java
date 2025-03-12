e2449dd8b9b2abcfa2331dfe8cf13d1c33624d08
rikardsv <rikardsv@stud.ntnu.no>
2025-03-12 11:16:33 +0100

    Changed name for clarity

--- src/main/java/board/Board.java
+++ src/main/java/board/Board.java
@@ -2,37 +2,37 @@ package board;
 
 public class Board {
   private final int size;
-  private final int[] board;
+  private final int[] gameBoard;
 
   public Board(int size) {
     this.size = size;
-    board = new int[size + 1];
+    gameBoard = new int[size + 1];
     initializeBoard();
     setupSpecialSquares();
   }
 
   private void initializeBoard() {
     for (int i = 1; i <= size; i++) {
-      board[i] = i;
+      gameBoard[i] = i;
     }
   }
 
   private void setupSpecialSquares() {
     // TODO: figure out where the special squares should be
     // ladders: square -> destination (higher number)
-    board[3] = 12;
-    board[7] = 19;
+    gameBoard[3] = 12;
+    gameBoard[7] = 19;
 
     // snakes: same thing (lower number)
-    board[15] = 1;
-    board[24] = 5;
+    gameBoard[15] = 1;
+    gameBoard[24] = 5;
   }
 
   public int getDestination(int square) {
     if (square < 1 || square > size) {
       throw new IllegalArgumentException("Invalid square number: " + square);
     }
-    return board[square];
+    return gameBoard[square];
   }
 
   public int movePlayer(int currentSquare, int diceRoll) {
