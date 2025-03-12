9b270eff66b0b753e025acb7a2c7dcb34beec839
rikardsv <rikardsv@stud.ntnu.no>
2025-03-12 12:53:11 +0100

    New tile class and interface
    Initial commits

---
+++ src/main/java/board/Tile.java
@@ -0,0 +1,18 @@
+package board;
+
+import gameplay.Player;
+
+public class Tile {
+
+    private Tile nextTIle;
+    private int tileId;
+    private TileAction landAction;
+
+    public Tile(int tileId){
+
+    }
+
+    public void landPlayer(Player player){
+
+    }
+}
