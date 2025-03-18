fd6b70e8a3f4b243e023974d6297fc65b2982af1
wayansk <wayansk@stud.ntnu.no>
2025-03-18 20:12:35 +0100

    added comment for more clarity

--- src/main/java/engine/DemoRunner.java
+++ src/main/java/engine/DemoRunner.java
@@ -2,6 +2,7 @@ package engine;
 
 /**
  * A simple file to test run the demo
+ * can also be considered as a manual test instead of automated JUnit test
  */
 public class  DemoRunner {
   public static void main(String[] args) {
