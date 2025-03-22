79cfad4e61a0e30d7f32ac464d45914d893c8c7f
rikardsv <rikardsv@stud.ntnu.no>
2025-03-22 13:26:40 +0100

    Changes to simplify the LadderAction class and the Tile class.

--- src/main/java/board/LadderAction.java
+++ src/main/java/board/LadderAction.java
@@ -7,37 +7,17 @@ import gameplay.Player;
  * This class implements the {@link TileAction} interface.
  */
 public class LadderAction implements TileAction {
+    private int fromTile;
+    private int toTile;
 
-    /**
-     * The ID of the destination tile where the player will be moved.
-     */
-    private final int destinationTileId;
-
-    /**
-     * A description of the ladder action.
-     */
-    private final String description;
-
-    /**
-     * Constructs a {@code LadderAction} with the specified destination tile ID and description.
-     *
-     * @param destinationTileId The ID of the tile to which the player will be moved.
-     * @param description A brief description of the ladder action.
-     */
-    public LadderAction(int destinationTileId, String description) {
-        this.destinationTileId = destinationTileId;
-        this.description = description;
+    public LadderAction(int fromTile, int toTile){
+        this.fromTile = fromTile;
+        this.toTile = toTile;
     }
 
-    /**
-     * Performs the ladder action, moving the player to the destination tile.
-     * This method currently prints the description but should be updated to modify the player's position.
-     *
-     * @param player The player on whom the action is performed.
-     */
     @Override
-    public void perform(Player player) {
-        System.out.println(description);
-        // TODO: Implement method to update player's position
+    public void perform(Player player){
+        // sysout
+        // perform(tofile)
     }
 }
