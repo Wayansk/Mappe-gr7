5038c21bd8adb6089d2e81c9025e538df7013783
rikardsv <rikardsv@stud.ntnu.no>
2025-03-24 19:26:52 +0100

    Deleted outdated classes and folders.
    Mostly GUI content, will be added later, after filesystem is set up.

--- src/main/java/main/GameMain.java
+++ src/main/java/main/GameMain.java
@@ -1,24 +1,8 @@
 package main;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+public class GameMain {
 
-public class GameMain extends Application {
-  @Override
-  public void start(Stage primaryStage) {
-    GamePanel gamePanel = new GamePanel();
-
-    StackPane root = new StackPane(gamePanel.getCanvas());
-    Scene scene = new Scene(root, 800, 600);
-
-    primaryStage.setTitle("placeholder text");
-    primaryStage.setScene(scene);
-    primaryStage.show();
-  }
-
-  public static void main(String[] args) {
-    launch(args);
+  public static void main(String[] args){
+    // Empty for now
   }
 }
