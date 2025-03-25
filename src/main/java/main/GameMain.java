cd4667c56dfa0c4c054a40477e71bd7f4b966208
rikardsv <rikardsv@stud.ntnu.no>
2025-03-25 20:22:56 +0100

    Javadoc for GameMain and Launcher classes

--- src/main/java/main/GameMain.java
+++ src/main/java/main/GameMain.java
@@ -1,8 +1,21 @@
 package main;
 
+import engine.BoardGame;
+
+/**
+ * Class that starts the application.
+ */
 public class GameMain {
 
-  public static void main(String[] args){
-    // Empty for now
+  /**
+   * Launches the BoardGame application. This method creates an instance of {@code BoardGame} and
+   * calls its {@code start()} method to begin the game.
+   *
+   * @param args command-line arguments (not used)
+   */
+  public static void main(String[] args) {
+    BoardGame boardGame = new BoardGame();
+    boardGame.start();
   }
+
 }
