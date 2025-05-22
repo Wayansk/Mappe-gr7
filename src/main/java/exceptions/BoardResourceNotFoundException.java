35448e02393f0233ffa156d5b61d08cef7b2596f
wayansk <wayansk@stud.ntnu.no>
2025-05-22 22:31:19 +0200

    created new exception and added throwable cause to BoardResourceNotFoundException

--- src/main/java/exceptions/BoardResourceNotFoundException.java
+++ src/main/java/exceptions/BoardResourceNotFoundException.java
@@ -4,4 +4,8 @@ public class BoardResourceNotFoundException extends RuntimeException {
   public BoardResourceNotFoundException(String message) {
     super(message);
   }
+
+  public BoardResourceNotFoundException(String message, Throwable cause) {
+    super(message, cause);
+  }
 }
